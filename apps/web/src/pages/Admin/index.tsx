57142819aba3f6cc32afeec559b6f06ace94e6fb
young-trigold <cluscandlot63@gmail.com>
2022-10-23 14:37:00 +0800

    feat: not logon open login modal

--- apps/web/src/pages/Admin/index.tsx
+++ apps/web/src/pages/Admin/index.tsx
@@ -1,25 +1,33 @@
 import { Navigate } from 'react-router-dom';
 
-import { AppState } from '@/app/store';
+import { AppDispatch, AppState } from '@/app/store';
 import { UserInfo } from '@/app/store/user';
 import { message } from '@/components/Message';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
 import { useSelector } from 'react-redux';
 import AdminPage from './components/AdminPage';
+import { useDispatch } from 'react-redux';
+import { setLoginModalVisible } from '@/app/store/modals';
 
 const ProtectPage: React.FC = () => {
 	useDocumentTitle('博客后台');
 	const user = useSelector((state: AppState) => state.user);
 	const { hasLogin, info } = user;
 
+  const dispatch = useDispatch<AppDispatch>();
+
 	if (hasLogin) {
 		if ((info as UserInfo).role === 'admin') return <AdminPage />;
-		message.warn('权限不足');
+		else {
+      dispatch(setLoginModalVisible(true));
+			message.warn('权限不足，请重新登录!');
+			return <Navigate to="/" />;
+		}
 	} else {
+    dispatch(setLoginModalVisible(true));
 		message.warn('请先登录');
+		return <Navigate to="/" />;
 	}
-
-	return <Navigate to="/" replace />;
 };
 
 export default ProtectPage;
